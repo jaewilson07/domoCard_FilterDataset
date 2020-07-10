d46df25c0f05591bcb427f454a677d75e0718a02
Jae Wilson <jaewilson07@gmail.com>
2020-07-10 16:44:03 +0200

    Updates

---
+++ precache-manifest.6db1e3f12ab84e427e7725fe71c52894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a03f38d9092822a6dcb73d9b4fe9cd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8454c6a3da25cfd18a4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae628c294eb61591c397",
+    "url": "/static/js/2.82fc34fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.82fc34fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8454c6a3da25cfd18a4",
+    "url": "/static/js/main.8045b671.chunk.js"
+  },
+  {
+    "revision": "4fcdff0673748c5d7f67",
+    "url": "/static/js/runtime-main.c2b22f29.js"
+  }
+]);
